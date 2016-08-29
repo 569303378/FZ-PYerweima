4576af8d9279f7f172317a02673274e720f78313
Apple <569303378@qq.com>
2016-08-29 14:38:35 +0800

    二维码生成

---
+++ PYerweima/PYerweima/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PYerweima
+//
+//  Created by Apple on 16/8/26.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PYerweima/PYerweima/ViewController.m
@@ -0,0 +1,100 @@
+//
+//  ViewController.m
+//  PYerweima
+//
+//  Created by Apple on 16/8/26.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreImage/CoreImage.h>//生成二维码
+
+@interface ViewController () 
+@property (weak, nonatomic) IBOutlet UIImageView *myImageView;
+@property (weak, nonatomic) IBOutlet UITextField *myTextField;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark = 生成二维码
+- (IBAction)PYerweimaButton:(UIButton *)sender {
+    [self creatCIQRCodeImage];
+}
+
+/**
+ *  生成二维码
+ */
+- (void)creatCIQRCodeImage
+{
+    // 1.创建过滤器，这里的@"CIQRCodeGenerator"是固定的
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    
+    // 2.恢复默认设置
+    [filter setDefaults];
+    
+    // 3. 给过滤器添加数据
+    NSString *dataString = self.myTextField.text;
+    NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    // 注意，这里的value必须是NSData类型
+    [filter setValue:data forKeyPath:@"inputMessage"];
+    
+    // 4. 生成二维码
+    CIImage *outputImage = [filter outputImage];
+    
+    // 5. 显示二维码
+    
+    self.myImageView.image = [self creatNonInterpolatedUIImageFormCIImage:outputImage withSize:200];
+    
+    
+}
+
+/**
+ *  根据CIImage生成指定大小的UIImage
+ *
+ *  @param image CIImage
+ *  @param size  图片宽度
+ *
+ *  @return 生成高清的UIImage
+ */
+- (UIImage *)creatNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat)size
+{
+    CGRect extent = CGRectIntegral(image.extent);
+    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+    
+    // 1. 创建bitmap
+    size_t width = CGRectGetWidth(extent) * scale;
+    size_t height = CGRectGetHeight(extent) * scale;
+    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+    CGContextScaleCTM(bitmapRef, scale, scale);
+    CGContextDrawImage(bitmapRef, extent, bitmapImage);
+    
+    // 2.保存bitmap图片
+    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+    CGContextRelease(bitmapRef);
+    CGImageRelease(bitmapImage);
+    return [UIImage imageWithCGImage:scaledImage];
+}
+
+#pragma mark = 扫描二维码
+- (IBAction)PYsaomiaoItem:(UIBarButtonItem *)sender {
+    
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
